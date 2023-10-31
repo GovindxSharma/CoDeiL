933335ed06866da9736772a80b5cfa3da3e8738e
GovindxSharma <govindsharma2839@gmail.com>
2023-10-31 21:32:12 +0530

    routing with controllers

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 const express=require('express')
 
 const router= express.Router();
+const ControllerHome=require('../controllers/home_controller')
 console.log('router is loaded')
+router.get('/',ControllerHome.home)
+router.use('/users',require('./users'))
+router.use('/users',require('./post'))
 
 module.exports=router;
